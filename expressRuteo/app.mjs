2fa74ab393fd5d6a375c1f34b423d2c720aa2c47
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2024-10-23 10:22:31 -0300

    Corrección

--- expressRuteo/app.mjs
+++ expressRuteo/app.mjs
@@ -15,30 +15,38 @@ app.get('/data', (req, res)=>{
 });
 
 //Rutas con parámetros
-
+//Solicitud: http://localhost:3000/user/123
 app.get('/user/:id', (req, res)=>{
     const userId = req.params.id; 
     res.send(`Usuario ingresado con el id: ${userId}`);
 })
 
+//Solicitud: http://localhost:3000/product/electronic/456
 app.get('/product/:category/:id', (req, res)=>{
     const {category, id} = req.params;
     res.send(`Categoria: ${category}, Id del producto: ${id}`);
 });
 
 //Ruteo de Consulta
+//Solicitud: http://localhost:3000/search?q=javascript
 app.get('/search', (req,res)=>{
     const query = req.query.q;
     res.send(`Resultado de la búsqueda: ${query}`)
 });
 
+
+//Solicitud: http://localhost:3000/filter?type=book&minPrice=10&maxPrice=50
 app.get('/filter', (req, res)=>{
     const {type, minPrice, maxPrice} = req.query;
     res.send(`Filtrar por tipo ${type}, rango de precios ${minPrice} - ${maxPrice}`)
 });
 
-
-
+//Solicitud: http://localhost:3000/profile?edad=30
+app.get('/profile', (req,res)=>{
+    const edad = req.query.edad; 
+    console.log(`La edad recibidad es: ${edad}`);
+    res.send(`Edad del perfil: ${edad}`)
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server running http://localhost:${PORT}`);
